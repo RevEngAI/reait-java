65d98cb14dcf409c33bf1e7e948ec5f503ee98f4
Nathan <nathan@reveng.ai>
2023-08-24 11:29:00 +0100

    add more tests

--- lib/src/test/java/ai/reveng/toolkit/api/ReaiApiProxyTest.java
+++ lib/src/test/java/ai/reveng/toolkit/api/ReaiApiProxyTest.java
@@ -2,6 +2,7 @@ package ai.reveng.toolkit.api;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.HashMap;
@@ -16,26 +17,38 @@ class ReaiApiProxyTest {
 	private Path workingDir;
 	private IApiRequester apiRequester;
 	private Map<String, String> headers;
+	private Map<String, String> params;
+	private Map<String, String> body;
+	private Map<String, String> pathParams;
+	private Config rc;
 
 	@BeforeEach
 	public void init() {
-		this.workingDir = Path.of("", "src/test/resources");
+		workingDir = Path.of("", "src/test/resources");
 		Path configFile = this.workingDir.resolve("reai-dev-config.toml");
-		Config rc = new Config(configFile.toString());
+		rc = new Config(configFile.toString());
 		assertEquals("f77d0f9e-c7f2-4652-bd53-256c1aa3bc78", rc.getApiKey());
 		assertEquals("https://api.reveng.ai", rc.getHost());
 		assertEquals("binnet-0.1", rc.getModel().toString());
 
 		apiRequester = new ReaiApiProxy(rc.getHost());
-		
+
 		headers = new HashMap<>();
 		headers.put("Authorization", rc.getApiKey());
+		headers.put("User-Agent", "REAIT Java Tests");
+
+		params = new HashMap<>();
+		
+		pathParams = new HashMap<>();
 	}
 
 	@Test
 	void testSendEcho() {
 		try {
-			ApiResponse res = apiRequester.send(ApiEndpoint.ECHO, null, null, headers);
+			ApiResponse res = apiRequester.send(ApiEndpoint.ECHO, null, null, // no params
+					null, // no body for GET
+					null, // no body type
+					headers);
 			assertEquals(200, res.getStatusCode());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -45,12 +58,31 @@ class ReaiApiProxyTest {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
-	void testSendGetModels() {
+	void testUpload() {
+		Path binPath = workingDir.resolve("false");
+		File bin = binPath.toFile();
+		assertTrue(bin.exists());
+
+		params.put("model", rc.getModel().toString());
+		params.put("file_options", "ELF");
+		params.put("file_name", bin.getName());
+		params.put("base_vaddr", "00400000");
+
 		try {
-			ApiResponse res = apiRequester.send(ApiEndpoint.GET_MODELS, null, null, headers);
+			// Upload the file
+			ApiResponse res = apiRequester.send(ApiEndpoint.ANALYSE, null, params, binPath, ApiBodyType.FILE, headers);
 			assertEquals(200, res.getStatusCode());
+			
+			String binHash = res.getJsonObject().getString("sha_256_hash");
+			pathParams.put("sha_256_hash", binHash);
+			// wait until status returns complete
+			res = apiRequester.send(ApiEndpoint.STATUS, pathParams, params, null, null, headers);
+			assertEquals(200, res.getStatusCode());
+			
+			// delete the file
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
